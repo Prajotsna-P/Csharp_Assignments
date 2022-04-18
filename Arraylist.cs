d8765108becfced5155df4664d01ad4c249a075f
Prajotsna-P <77489461+Prajotsna-P@users.noreply.github.com>
2022-04-18 11:39:12 +0530

    Add files via upload

---
+++ Arraylist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSal { get; set; }
+        public string EmpLoc { get; set; }
+
+
+    }
+
+    internal class ArrayListSample
+    {
+        static void Main(string[] args)
+        {
+            //object initilizer or collection initilizer
+            //ArrayList e = new ArrayList();//non generic
+
+            ArrayList e = new ArrayList()
+            {
+                    new Employee{EmpId=1,EmpName="Sirinisha",EmpSal=25000,EmpLoc="Hyd"},
+                    new Employee{EmpId=2,EmpName="Nian",EmpSal=15000,EmpLoc="Hyd"},
+                    new Employee{EmpId=3,EmpName="Williams",EmpSal=30000,EmpLoc="Hyd"},
+                    new Employee{EmpId=3,EmpName="Vikranth",EmpSal=38000,EmpLoc="Hyd"},
+                    new Employee{ EmpId=4, EmpName="Vinnesha"EmpSal=40000EmpLoc="Hyd",},
+            };
+            foreach (Employee i in e)
+            {
+                Console.WriteLine(i.EmpId + " " + i.EmpName + " " + i.EmpSal + " " + i.EmpLoc);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Employee_stack.cs
@@ -0,0 +1,21 @@
+﻿static void Main()
+{
+    Stack stack1 = new Stack();
+    stack1.Push(1);
+    stack1.Push("Employee");
+    stack1.Push("13286")
+    stack1.Push("Miana");
+    stack1.Push("30000");
+    foreach (Object obj in stack1)
+    {
+        Console.WriteLine("different types of data" + ":" + obj);
+    }
+    stack1.Pop();
+    foreach (Object obj in stack1)
+    {
+        Console.WriteLine("after pop operation" + ":" + obj);
+    }
+
+    Console.ReadLine();
+}
+    }
