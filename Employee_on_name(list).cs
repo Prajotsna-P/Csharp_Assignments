f0fff9a60e36b6b0c8f901d196f6535db2cfa4a5
Prajotsna-P <77489461+Prajotsna-P@users.noreply.github.com>
2022-04-18 11:45:53 +0530

    Add files via upload

---
+++ Employee_on_name(list).cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeeonname;
+    internal class Search
+{
+    static void Main()
+    {
+        List<string> list = new List<string>();
+        list.Add("Namitha");
+        list.Add("Lucifer");
+        list.Add("Viyaan");
+        list.Add("Kruthika");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine("List of employees" + ':' + list[i]);
+        }
+        Console.WriteLine("total number of employess" + ':' + list.Count);
+        Console.WriteLine("enter person name:");
+        string X = Console.ReadLine();
+        if (list.Contains(X))
+        {
+            Console.WriteLine("yes " + X + " is an employee");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+
+
+
+        Console.ReadLine();
+    }
+}
+}
